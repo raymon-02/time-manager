8b4319983b30e972228660ecf30e5fe74d7e3820
Ilia <2x2.4@mail.ru>
2016-01-09 22:17:31 +0300

    Add LoginForm

---
+++ src/LoginForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace TimeManager
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.signinButton = new System.Windows.Forms.Button();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(60, 150);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(48, 20);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(64, 173);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(154, 26);
+            this.loginTextBox.TabIndex = 1;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(60, 222);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(78, 20);
+            this.passwordLabel.TabIndex = 2;
+            this.passwordLabel.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(64, 245);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(154, 26);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // signinButton
+            // 
+            this.signinButton.Location = new System.Drawing.Point(64, 317);
+            this.signinButton.Name = "signinButton";
+            this.signinButton.Size = new System.Drawing.Size(90, 40);
+            this.signinButton.TabIndex = 4;
+            this.signinButton.Text = "button1";
+            this.signinButton.UseVisualStyleBackColor = true;
+            // 
+            // signupButton
+            // 
+            this.signupButton.Location = new System.Drawing.Point(160, 317);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(90, 40);
+            this.signupButton.TabIndex = 5;
+            this.signupButton.Text = "button1";
+            this.signupButton.UseVisualStyleBackColor = true;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(256, 317);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(90, 40);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "button1";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.ClientSize = new System.Drawing.Size(403, 459);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.signinButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.loginLabel);
+            this.MaximumSize = new System.Drawing.Size(425, 515);
+            this.MinimumSize = new System.Drawing.Size(425, 515);
+            this.Name = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button signinButton;
+        private System.Windows.Forms.Button signupButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
