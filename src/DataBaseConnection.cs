c490f92fbc1cbbf82c1827820012df4ec0f55c08
Ilia <2x2.4@mail.ru>
2016-01-10 00:23:41 +0300

    Add facade for DataBaseConnection

---
+++ src/DataBaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace TimeManager
+{
+    class DataBaseConnection
+    {
+        private const string ServerDataBase = "Server=localhost";
+        private const string PortDataBase = "Port=5432";
+        private const string UserIdDataBase = "User Id=postgres";
+        private const string PasswordDataBase = "Password=csc";
+        private const string DataBase = "Database=postgres";
+
+        private DataBaseConnection() { }
+
+        public static NpgsqlConnection GetConnection()
+        {
+            var settingsDataBase = new string[] { ServerDataBase, PortDataBase, UserIdDataBase, PasswordDataBase, DataBase };
+            var connectionDataBase = string.Join(";", settingsDataBase);
+            return new NpgsqlConnection(connectionDataBase);
+        }
+    }
+}
