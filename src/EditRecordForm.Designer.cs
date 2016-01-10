736ce382ea56d2152f6e364a874a371f5b75f26f
Ilia <2x2.4@mail.ru>
2016-01-10 22:39:39 +0300

    Add functional to EditRecordForm

--- src/EditRecordForm.Designer.cs
+++ src/EditRecordForm.Designer.cs
@@ -28,20 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.startTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.startTimeLabel = new System.Windows.Forms.Label();
+            this.endTimeLabel = new System.Windows.Forms.Label();
+            this.endTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.categoryBox = new System.Windows.Forms.ComboBox();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // startTimePicker
+            // 
+            this.startTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.startTimePicker.Location = new System.Drawing.Point(58, 142);
+            this.startTimePicker.Margin = new System.Windows.Forms.Padding(4);
+            this.startTimePicker.Name = "startTimePicker";
+            this.startTimePicker.Size = new System.Drawing.Size(156, 37);
+            this.startTimePicker.TabIndex = 0;
+            this.startTimePicker.Value = new System.DateTime(2016, 1, 10, 0, 0, 0, 0);
+            // 
+            // startTimeLabel
+            // 
+            this.startTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startTimeLabel.Location = new System.Drawing.Point(54, 102);
+            this.startTimeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.startTimeLabel.Name = "startTimeLabel";
+            this.startTimeLabel.Size = new System.Drawing.Size(130, 36);
+            this.startTimeLabel.TabIndex = 1;
+            this.startTimeLabel.Text = "Start Time";
+            this.startTimeLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // endTimeLabel
+            // 
+            this.endTimeLabel.AutoSize = true;
+            this.endTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.endTimeLabel.Location = new System.Drawing.Point(54, 210);
+            this.endTimeLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.endTimeLabel.Name = "endTimeLabel";
+            this.endTimeLabel.Size = new System.Drawing.Size(118, 29);
+            this.endTimeLabel.TabIndex = 2;
+            this.endTimeLabel.Text = "End Time";
+            this.endTimeLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // endTimePicker
+            // 
+            this.endTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.endTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.endTimePicker.Location = new System.Drawing.Point(58, 242);
+            this.endTimePicker.Margin = new System.Windows.Forms.Padding(4);
+            this.endTimePicker.Name = "endTimePicker";
+            this.endTimePicker.Size = new System.Drawing.Size(156, 37);
+            this.endTimePicker.TabIndex = 3;
+            this.endTimePicker.Value = new System.DateTime(2016, 1, 10, 0, 0, 0, 0);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(58, 426);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 43);
+            this.okButton.TabIndex = 4;
+            this.okButton.Text = "OK";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(139, 426);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 43);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // categoryBox
+            // 
+            this.categoryBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryBox.FormattingEnabled = true;
+            this.categoryBox.Location = new System.Drawing.Point(58, 347);
+            this.categoryBox.Name = "categoryBox";
+            this.categoryBox.Size = new System.Drawing.Size(156, 28);
+            this.categoryBox.TabIndex = 6;
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryLabel.Location = new System.Drawing.Point(54, 315);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(110, 29);
+            this.categoryLabel.TabIndex = 7;
+            this.categoryLabel.Text = "Category";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(250, 70);
+            this.titleLabel.TabIndex = 8;
+            this.titleLabel.Text = " record for date:\r\n";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // EditRecordForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 383);
+            this.ClientSize = new System.Drawing.Size(274, 500);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.categoryBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.endTimePicker);
+            this.Controls.Add(this.endTimeLabel);
+            this.Controls.Add(this.startTimeLabel);
+            this.Controls.Add(this.startTimePicker);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "EditRecordForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "EditRecordForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker startTimePicker;
+        private System.Windows.Forms.Label startTimeLabel;
+        private System.Windows.Forms.Label endTimeLabel;
+        private System.Windows.Forms.DateTimePicker endTimePicker;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ComboBox categoryBox;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
--- src/EditRecordForm.cs
+++ src/EditRecordForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -12,9 +13,69 @@ namespace TimeManager
 {
     public partial class EditRecordForm : Form
     {
-        public EditRecordForm()
+        private readonly IDictionary<string, long> categories;
+        private string startTime;
+        private string endTime;
+        private readonly string date;
+
+        public EditRecordForm(IDictionary<string, long> categories, string date)
         {
             InitializeComponent();
+
+            this.categories = categories;
+            this.date = date;
+
+            initForm("Add");
+        }
+        public EditRecordForm(IDictionary<string, long> categories, string startTime, string endTime, string date)
+        {
+            InitializeComponent();
+
+            this.categories = categories;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.date = date;
+
+            initForm("Edit");
+        }
+
+        private void initForm(string action)
+        {
+            this.Text = action + @" record";
+            titleLabel.Text = action + titleLabel.Text + date;
+
+            foreach (var category in categories.Keys)
+            {
+                categoryBox.Items.Add(category);
+            }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            if (categoryBox.Text == "")
+            {
+                MessageBox.Show(@"Category field must be not empty", @"Category field", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            var result = MessageBox.Show(@"Save changes?", @"Saving changes", 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            startTime = startTimePicker.Text;
+            endTime = endTimePicker.Text;
+            DialogResult = DialogResult.OK;
+
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
