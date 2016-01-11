cbc362089e7593bfb778414fab0596250a8e8945
Ilia <2x2.4@mail.ru>
2016-01-11 08:52:07 +0300

    Add StandartCategoryForm

---
+++ src/StandartCategoryForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TimeManager
+{
+    partial class StandartCategoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataListBox = new System.Windows.Forms.CheckedListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dataListBox
+            // 
+            this.dataListBox.FormattingEnabled = true;
+            this.dataListBox.Location = new System.Drawing.Point(43, 54);
+            this.dataListBox.Name = "dataListBox";
+            this.dataListBox.Size = new System.Drawing.Size(231, 319);
+            this.dataListBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(43, 400);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(109, 48);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(158, 400);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(116, 48);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // StandartCategoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(323, 482);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.dataListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "StandartCategoryForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Standart categories";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox dataListBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ src/StandartCategoryForm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace TimeManager
+{
+    public partial class StandartCategoryForm : Form
+    {
+        private readonly NpgsqlConnection npgsqlConnection;
+
+        private readonly int id;
+        public StandartCategoryForm(int id)
+        {
+            InitializeComponent();
+
+            npgsqlConnection = DataBaseConnection.GetConnection();
+            npgsqlConnection.Open();
+
+            this.id = id;
+
+            initData();
+        }
+
+        private void initData()
+        {
+            var query = "SELECT name FROM Category ORDER BY name";
+
+            var cmd = new NpgsqlCommand(query, npgsqlConnection);
+            var dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dataListBox.Items.Add(dr[0]);
+            }
+
+            dr.Close();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show(@"Add selected categories?", @"Adding categories", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            IDictionary<string, int> categories = new Dictionary<string, int>();
+
+            foreach (var item in dataListBox.CheckedItems)
+            {
+                var query = "SELECT id FROM Category" +
+                            " WHERE name = '" + item.ToString() + "'";
+
+                var cmd = new NpgsqlCommand(query, npgsqlConnection);
+                var dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    categories.Add(item.ToString(), dr.GetInt32(0));
+                }
+
+                dr.Close();
+            }
+            
+            foreach (var i in categories.Values)
+            {
+                var query = "SELECT id FROM Mem_cat" +
+                            " WHERE member_id=" + id + " AND category_id=" + i;
+
+                var cmd = new NpgsqlCommand(query, npgsqlConnection);
+                var dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    dr.Close();
+                    continue;
+                }
+                dr.Close();
+
+                query = "INSERT INTO Mem_cat (member_id, category_id)" + 
+                            " VALUES (" + id + ", " + i + ")";
+
+                cmd = new NpgsqlCommand(query, npgsqlConnection);
+                dr = cmd.ExecuteReader();
+                dr.Close();
+            }
+
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            npgsqlConnection.Close();
+        }
+    }
+}
