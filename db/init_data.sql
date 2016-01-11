2ed8afc3a3fdd3b4c042b80c9d654f54cb0db6a6
Ilia <2x2.4@mail.ru>
2016-01-11 08:51:07 +0300

    Add standart categories

--- db/init_data.sql
+++ db/init_data.sql
@@ -1,1 +1,6 @@
 INSERT INTO Member (role, login, password) VALUES ('admin', 'admin', 'admin');
+
+INSERT INTO Category (name) 
+    VALUES ('Sleep'), ('Meal'), ('Work'), 
+           ('Entertainment'), ('Rest'), ('Housework'), 
+           ('Coding');
