5656151af59c0b9ad9a0e5c2561ed55dc2d62484
Ilia <2x2.4@mail.ru>
2016-01-11 09:30:08 +0300

    Add RemoveCategoryForm

---
+++ src/RemoveCategoryForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TimeManager
+{
+    partial class RemoveCategoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataListBox = new System.Windows.Forms.CheckedListBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dataListBox
+            // 
+            this.dataListBox.FormattingEnabled = true;
+            this.dataListBox.Location = new System.Drawing.Point(44, 53);
+            this.dataListBox.Name = "dataListBox";
+            this.dataListBox.Size = new System.Drawing.Size(259, 298);
+            this.dataListBox.TabIndex = 0;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(44, 375);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(125, 46);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(175, 375);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(128, 46);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // RemoveCategoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 459);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.dataListBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "RemoveCategoryForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Remove Categories";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox dataListBox;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ src/RemoveCategoryForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace TimeManager
+{
+    public partial class RemoveCategoryForm : Form
+    {
+        private readonly NpgsqlConnection npgsqlConnection;
+
+        private readonly int id;
+        private readonly IDictionary<string, long> categories; 
+        public RemoveCategoryForm(int id, IDictionary<string, long> categories)
+        {
+            InitializeComponent();
+
+            npgsqlConnection = DataBaseConnection.GetConnection();
+            npgsqlConnection.Open();
+
+            this.id = id;
+            this.categories = categories;
+
+            initData();
+        }
+
+        private void initData()
+        {
+            foreach (var e in categories.Keys)
+            {
+                dataListBox.Items.Add(e);
+            }
+            
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show(@"Remove selected categories?", @"Removing categories", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            foreach (var item in dataListBox.CheckedItems)
+            {
+                var query = "SELECT Mem_cat.id FROM Mem_cat" +
+                            " JOIN Category ON (Mem_cat.category_id=Category.id)" +
+                            " WHERE Mem_cat.member_id=" + id +
+                            " AND Category.name='" + item.ToString() + "'";
+
+                var cmd = new NpgsqlCommand(query, npgsqlConnection);
+                var dr = cmd.ExecuteReader();
+                dr.Read();
+                var idMemCat = dr[0];
+                dr.Close();
+
+                query = "DELETE FROM Data WHERE mem_cat_id=" + idMemCat;
+                cmd = new NpgsqlCommand(query, npgsqlConnection);
+                dr = cmd.ExecuteReader();
+                dr.Close();
+
+                query = "DELETE FROM Mem_cat WHERE id=" + idMemCat;
+                cmd = new NpgsqlCommand(query, npgsqlConnection);
+                dr = cmd.ExecuteReader();
+                dr.Close();
+            }
+            
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            npgsqlConnection.Close();
+        }
+    }
+}
