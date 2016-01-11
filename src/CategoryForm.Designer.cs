e9c2ae59a27f7c8d85d17e03c9a573a4f821bcce
Ilia <2x2.4@mail.ru>
2016-01-11 08:51:57 +0300

    Add CategoryForm

---
+++ src/CategoryForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TimeManager
+{
+    partial class CategoryForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.categoryTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryLabel.Location = new System.Drawing.Point(53, 84);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(161, 29);
+            this.categoryLabel.TabIndex = 0;
+            this.categoryLabel.Text = "New category";
+            // 
+            // categoryTextBox
+            // 
+            this.categoryTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.categoryTextBox.Location = new System.Drawing.Point(58, 116);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(226, 37);
+            this.categoryTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(58, 192);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(109, 44);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(173, 192);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(111, 44);
+            this.cancelButton.TabIndex = 3;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // CategoryForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 301);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.categoryTextBox);
+            this.Controls.Add(this.categoryLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CategoryForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New category";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TextBox categoryTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ src/CategoryForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace TimeManager
+{
+    public partial class CategoryForm : Form
+    {
+        private readonly NpgsqlConnection npgsqlConnection;
+
+        private readonly int id;
+
+        public CategoryForm(int id)
+        {
+            InitializeComponent();
+
+            npgsqlConnection = DataBaseConnection.GetConnection();
+            npgsqlConnection.Open();
+
+            this.id = id;
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show(@"Add new category?", @"Adding category", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            var name = categoryTextBox.Text;
+            var idCategory = 0;
+
+            var query = "SELECT id FROM Category WHERE name='" + name + "'";
+            var cmd = new NpgsqlCommand(query, npgsqlConnection);
+            var dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                idCategory = dr.GetInt32(0);
+                dr.Close();
+
+                query = "SELECT id FROM Mem_cat WHERE member_id=" + id + " AND category_id=" + idCategory;
+                cmd = new NpgsqlCommand(query, npgsqlConnection);
+                dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    dr.Close();
+                    Close();
+                    return;
+                }
+                dr.Close();
+            }
+            else
+            {
+                dr.Close();
+                query = "INSERT INTO Category (name) VALUES ('" + name + "')";
+                cmd = new NpgsqlCommand(query, npgsqlConnection);
+                dr = cmd.ExecuteReader();
+                dr.Close();
+
+                query = "SELECT id FROM Category WHERE name='" + name + "'";
+                cmd = new NpgsqlCommand(query, npgsqlConnection);
+                dr = cmd.ExecuteReader();
+                dr.Read();
+                idCategory = dr.GetInt32(0);
+                dr.Close();
+            }
+
+            query = "INSERT INTO Mem_cat (member_id, category_id)" +
+                            " VALUES (" + id + ", " + idCategory + ")";
+
+            cmd = new NpgsqlCommand(query, npgsqlConnection);
+            dr = cmd.ExecuteReader();
+            dr.Close();
+
+            Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            npgsqlConnection.Close();
+        }
+    }
+}
